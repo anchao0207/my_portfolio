6f8a950816b6cd9403b007fc08abc73aef271356
An <atrieu@knox.edu>
2023-01-19 10:06:23 -0600

    add in sections

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,8 @@ function App() {
 
       <div className="page">
         <div className="page-wrapper">
-
-          <div className="hero">
+          <section className="hero">
             <div className="hero-wrapper">
-
               <div className="hero-main">
                 <h1 className="hero-main-title">AN TRIEU</h1>
                 <p className="hero-main-desc">DECS</p>
@@ -29,9 +27,82 @@ function App() {
                 <p className="hero-second-subheading">An Trieu</p>
                 <p className="hero-second-subheading">Portfolio</p>
               </div>
-
             </div>
-          </div>
+          </section>
+
+          <div className="section-margin"></div>
+
+          <section className="first-section section left">
+            <div className="section-intro-wrapper">
+              <h1 className="section-title">
+                <span className="section-title-text">About Me</span>
+                <div className="section-title-decoration styleOne"></div>
+                <div className="section-title-decoration styleTwo"></div>
+                <div className="section-title-decoration styleThree"></div>
+              </h1>
+              <span className="section-number">01</span>
+            </div>
+            <div className="section-detail-wrapper">
+              <h3 className="section-heading">Lorem Ipsum</h3>
+              <p className="section-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptate rerum voluptas qui voluptates culpa accusantium maxime
+                ea dolore expedita molestias! Quibusdam deserunt necessitatibus
+                corrupti similique at numquam vitae, ab ex.
+              </p>
+              <h3 className="section-heading">Lorem Ipsum</h3>
+              <p className="section-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptate rerum voluptas qui voluptates culpa accusantium maxime
+                ea dolore expedita molestias! Quibusdam deserunt necessitatibus
+                corrupti similique at numquam vitae, ab ex.
+              </p>
+              <h3 className="section-heading">Lorem Ipsum</h3>
+              <p className="section-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptate rerum voluptas qui voluptates culpa accusantium maxime
+                ea dolore expedita molestias! Quibusdam deserunt necessitatibus
+                corrupti similique at numquam vitae, ab ex.
+              </p>
+            </div>
+          </section>
+
+          <div className="section-margin"></div>
+
+          <section className="second-section section right">
+            <div className="section-intro-wrapper">
+              <h1 className="section-title">
+              <span className="section-title-text">My Work</span>
+                <div className="section-title-decoration styleOne"></div>
+                <div className="section-title-decoration styleTwo"></div>
+                <div className="section-title-decoration styleThree"></div>
+              </h1>
+              <span className="section-number">01</span>
+            </div>
+            <div className="section-detail-wrapper">
+              <h3 className="section-heading">Lorem Ipsum</h3>
+              <p className="section-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptate rerum voluptas qui voluptates culpa accusantium maxime
+                ea dolore expedita molestias! Quibusdam deserunt necessitatibus
+                corrupti similique at numquam vitae, ab ex.
+              </p>
+              <h3 className="section-heading">Lorem Ipsum</h3>
+              <p className="section-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptate rerum voluptas qui voluptates culpa accusantium maxime
+                ea dolore expedita molestias! Quibusdam deserunt necessitatibus
+                corrupti similique at numquam vitae, ab ex.
+              </p>
+              <h3 className="section-heading">Lorem Ipsum</h3>
+              <p className="section-text">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Voluptate rerum voluptas qui voluptates culpa accusantium maxime
+                ea dolore expedita molestias! Quibusdam deserunt necessitatibus
+                corrupti similique at numquam vitae, ab ex.
+              </p>
+            </div>
+          </section>
         </div>
       </div>
     </>
